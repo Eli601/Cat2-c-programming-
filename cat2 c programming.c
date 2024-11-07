eac8d6b5c1115575ec2251059a116b3d7c78f4de
Eli601 <eliudkairu7@gmail.com>
2024-11-07 17:18:59 +0300

    Add files via upload

---
+++ cat2 c programming.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int scores[2][2]={{65,92},{84,72}};
+  
+  for(int i=0;i<2;i++){
+  for(int j=0;j<2;j++){
+  printf("%d\n",scores [i][j]);
+  }
+  }
+  return 0;
+  }
